d5dfb5e42a851148f52250d6a377cb35673abcdd
Guilherme Ferreira <gadd.ferreira@gmail.com>
2017-05-29 15:57:48 -0300

    base files

---
+++ Makefile
@@ -0,0 +1,2 @@
+build:
+	g++ -std=c++14 -Wall -Wpedantic src/main.cpp -o build/core
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+
+int main(int argc, char** argv) {
+    std::cout << "Stub" << std::endl;
+    return 0; 
+}
