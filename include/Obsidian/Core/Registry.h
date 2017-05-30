498da54549f65ecf785f0b32ffc69e598c258a38
Guilherme Ferreira <gadd.ferreira@gmail.com>
2017-05-30 18:35:28 -0300

    Removed repeated stuff

--- include/Obsidian/Core/Registry.h
+++ include/Obsidian/Core/Registry.h
@@ -1,15 +1,5 @@
 #ifndef _OBSIDIAN_CORE_REGISTRY_
 #define _OBSIDIAN_CORE_REGISTRY_
-    #define OBSIDIAN_VERSION 0.0.1
-
-    #ifdef __linux__ 
-        #define LIKELY(x)   __builtin_expect((x),1)
-        #define UNLIKELY(x) __builtin_expect((x),0)
-    #elif _WIN32
-        #define LIKELY(x)   x
-        #define UNLIKELY(x) x
-    #endif
-    
 
 #include "Obsidian/Util/Loggable.h"
 
