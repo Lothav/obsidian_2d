1649676ca6f8e22ab8a15d03ca48f6c4d2ea4de9
Guilherme Ferreira <gadd.ferreira@gmail.com>
2017-06-07 18:23:20 -0300

    Basic windowEvent class

---
+++ include/Obsidian2D/Core/WidowEvent.h
@@ -0,0 +1,20 @@
+#ifndef _OBSIDIAN2D_CORE_WIDOWEVENT_
+#define _OBSIDIAN2D_CORE_WIDOWEVENT_
+
+#include "Obsidian2D/Util/Loggable.h"
+
+namespace Obsidian2D
+{
+    namespace Core
+    {
+        enum WidowEvent
+        {
+            Click   = 1,
+            MoveIn  = 2,
+            MoveOut = 3,
+            Resize  = 4
+        }
+    }
+}
+
+#endif //_OBSIDIAN2D_CORE_WIDOWEVENT_
