0b1671cbf7c42e51f3983632992f01adeef9ca77
Guilherme Ferreira <gadd.ferreira@gmail.com>
2017-08-22 23:55:58 -0300

    added bg sound in mp3 for now, Position now is a Composition instead of hierarchy

--- src/Player.h
+++ src/Player.h
@@ -1,18 +1,19 @@
 #ifndef _PlayerClass_
 #define _PlayerClass_
 
-#include "Positionable.h"
+#include "Position.h"
 #include "Animation.h"
 
-class Player : public Positionable
+class Player
 {
 private:
 protected:
-    Animation animation;
-
     sf::Sprite sprite;
 
 public:
+    Animation animation;
+    Position position;
+
     Player()
     {
         animation.setBasePath("assets/characters/cowboy");
@@ -51,31 +52,31 @@ public:
     bool update(unsigned long long elapsedTime)
     {
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-            this->move({2.f, 2.f});
+            this->position.move({2.f, 2.f});
             animation.setAnimation("walk/right_down");
         } else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-            this->move({2.f, -2.f});
+            this->position.move({2.f, -2.f});
             animation.setAnimation("walk/right_up");
         } else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-            this->move({-2.f, 2.f});
+            this->position.move({-2.f, 2.f});
             animation.setAnimation("walk/left_down");
         } else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-            this->move({-2.f, -2.f});
+            this->position.move({-2.f, -2.f});
             animation.setAnimation("walk/left_up");
         } else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-            this->move({-3.f, 0.f});
+            this->position.move({-3.f, 0.f});
             animation.setAnimation("walk/left");
         } else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-            this->move({0.f, -3.f});
+            this->position.move({0.f, -3.f});
             animation.setAnimation("walk/up");
         } else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-            this->move({0.f, 3.f});
+            this->position.move({0.f, 3.f});
             animation.setAnimation("walk/down");
         } else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-            this->move({3.f, 0.f});
+            this->position.move({3.f, 0.f});
             animation.setAnimation("walk/right");
         } else {
-            vec2f lastMove = this->getLastMove();
+            vec2f lastMove = this->position.getLastMove();
             if(lastMove.x > 0 && lastMove.y > 0) {
                 animation.setAnimation("stand/v2/right_down");
             } else if(lastMove.x > 0 && lastMove.y < 0) {
@@ -102,7 +103,7 @@ public:
     const sf::Sprite& getSprite()
     {
         this->sprite.setTexture(this->animation.getCurrentFrame());
-        this->sprite.setPosition({this->getPosition().x, this->getPosition().y});
+        this->sprite.setPosition({this->position.getPosition().x, this->position.getPosition().y});
         return this->sprite;
     }
 };
