7c266c2cb26a91280eaba6d6e4383c6f9f4e2e55
Luiz Otavio <luizotrv@gmail.com>
2017-09-05 07:42:26 -0300

    vertex Buffer

--- include/Obsidian2D/Renderer/Window.h
+++ include/Obsidian2D/Renderer/Window.h
@@ -40,14 +40,8 @@ namespace Obsidian2D
 				vkDestroyPipeline(device, vkPipeline, NULL);
 				vkDestroyPipelineCache(device, pPipelineCache, NULL);
 
-				// Destroy buffers
-
-				for(auto v_buff : vertex_buffer)
-				{
-					delete v_buff;
-				}
-
 				delete descriptor_set;
+				delete vertex_buffer;
 
 				// Destroy shaders
 				vkDestroyShaderModule(device, shaderStages[0].module, NULL);
@@ -143,7 +137,7 @@ namespace Obsidian2D
 
 			RenderPass* 							render_pass;
 		protected:
-            std::vector<VertexBuffer *>             vertex_buffer;
+            VertexBuffer *             				vertex_buffer;
 			DescriptorSet* 							descriptor_set;
 
 			void createInstance()
@@ -176,19 +170,6 @@ namespace Obsidian2D
 
 				VkResult res = vkCreateInstance(&_inst_info, NULL, &instance);
 				assert(res == VK_SUCCESS);
-			}
-
-			void createVertexBuffer(std::vector<Vertex> _vertexData)
-			{
-				struct BufferData vertexBufferData = {};
-
-				vertexBufferData.device            = device;
-				vertexBufferData.usage             = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
-				vertexBufferData.physicalDevice    = gpu_vector[0];
-				vertexBufferData.properties        = VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT;
-				vertexBufferData.size              = _vertexData.size() * sizeof(Vertex);
-
-				vertex_buffer.push_back(new VertexBuffer(vertexBufferData));
 			}
 
 			void createLogicalDeviceAndCommandBuffer()
@@ -313,6 +294,20 @@ namespace Obsidian2D
 				descriptor_set->create(ds_params);
 
 
+				/* Vertex Buffer */
+
+				struct BufferData vertexBufferData = {};
+
+				vertexBufferData.device            = device;
+				vertexBufferData.usage             = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+				vertexBufferData.physicalDevice    = gpu_vector[0];
+				vertexBufferData.properties        = VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT;
+				vertexBufferData.size              = vertexData.size() * sizeof(Vertex);
+
+				vertex_buffer = new VertexBuffer(vertexBufferData);
+
+
+
 				// Vertex shader
 				shaderStages[0].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
 				// Set pipeline stage for this shader
@@ -333,7 +328,6 @@ namespace Obsidian2D
 				shaderStages[1].pName = "main";
 				assert(shaderStages[1].module != VK_NULL_HANDLE);
 
-                this->createVertexBuffer(vertexData);
 
 				VkVertexInputBindingDescription vi_binding;
 				vi_binding.binding 										= 0;
@@ -561,8 +555,8 @@ namespace Obsidian2D
 
 					const VkDeviceSize offsets[1] = {0};
 
-					std::vector<VkBuffer> buffers = VertexBuffer::getBuffersFromVector(vertex_buffer);
-					vkCmdBindVertexBuffers(command_buffer[i], 0, static_cast<uint32_t >(buffers.size()), buffers.data(), offsets);
+					//std::vector<VkBuffer> buffers = VertexBuffer::getBuffersFromVector(vertex_buffer);
+					vkCmdBindVertexBuffers(command_buffer[i], 0, 1, &vertex_buffer->buf, offsets);
 
 					this->init_viewports(command_buffer[i]);
 					this->init_scissors(command_buffer[i]);
